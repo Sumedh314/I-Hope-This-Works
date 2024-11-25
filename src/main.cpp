b81204388c5399e1a0a1dc4cec344b34df9e2575
Sumedh314 <sumedh3141@gmail.com>
2024-11-25 11:02:56 -0600

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,9 @@ void disabled() {
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize() {
+	
+}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
