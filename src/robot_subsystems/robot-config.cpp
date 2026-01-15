46de42564570f0c9b2eff63f5a44822b4858710a
c-motherwell <154465011+c-motherwell@users.noreply.github.com>
2026-01-15 16:55:29 -0600

    Update robot-config.cpp

--- src/robot_subsystems/robot-config.cpp
+++ src/robot_subsystems/robot-config.cpp
@@ -2,25 +2,25 @@
 #include "robot_subsystems/robot-config.hpp"
 
 // Motors
-pros::Motor front_left(-13, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
-pros::Motor middle_left(-12, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
-pros::Motor back_left(-11, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
-pros::Motor front_right(18, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
-pros::Motor middle_right(19, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
-pros::Motor back_right(20, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor front_left(-18, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor middle_left(-19, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor back_left(-20, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor front_right(13, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor middle_right(12, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor back_right(14, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
 
-pros::Motor intake(1, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
-pros::Motor hopper(2, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
-pros::Motor redirect(3, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor intake(5, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor hopper(4, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
+pros::Motor redirect(6, pros::v5::MotorGearset::blue, pros::v5::MotorEncoderUnits::degrees);
 
 
 // Sensors
-pros::IMU inertial(17);
+pros::IMU inertial(1);
 pros::adi::DigitalIn auton_select(2);
-pros::Rotation vertical(15);
-pros::Rotation horizontal(14);
+pros::Rotation vertical(2);
+pros::Rotation horizontal(3);
 // <<<<<<< HEAD
-pros::adi::Ultrasonic distance(7, 8);
+// pros::adi::Ultrasonic distance(7, 8);
 // =======
 // pros::adi::Ultrasonic distance(7, 8);
 // >>>>>>> 2d648702d6b4fa138a9758c030fb603fd77a9afb
